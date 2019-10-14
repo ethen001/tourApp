2efb39b294d1a91b03ba6c2fe44fc67e507e5b14
EngerThen <engerthen4d17@gmail.com>
2019-10-13 22:43:17 -0400

    removed unnecessary files

--- src/components/common/Icons.js
+++ src/components/common/Icons.js
@@ -1,23 +1,9 @@
 const Icons = {
-  //Menu : require('@assets/icons/menu-thin.png'),
   Camera:require('@assets/icons/camera.png'),
   User:require('@assets/icons/user.png'),
   facebook:require('@assets/icons/facebook.png'),
   instagram:require('@assets/icons/instagram.png'),
   twitter:require('@assets/icons/twitter.png'),
-  Home:require('@assets/icons/home.png'),
-  Itinerary:require('@assets/icons/itinerary.png'),
-  Lost:require('@assets/icons/lost.png'),
-  Message:require('@assets/icons/message.png'),
-  Keller:require('@assets/icons/Keller.png'),
-  Write:require('@assets/icons/writeMessage.png'),
-  leftArrow:require('@assets/icons/leftarrow-white.png'),
-  MenuWhite: require('@assets/icons/menu-white.png'),
-  UserWhite:require('@assets/icons/user-white.png'),
-  HomeWhite:require('@assets/icons/home-white.png'),
-  ItineraryWhite:require('@assets/icons/itinerary-white.png'),
-  LostWhite:require('@assets/icons/lost-white.png'),
-  MessageWhite:require('@assets/icons/message-white.png')
 
 };
 
